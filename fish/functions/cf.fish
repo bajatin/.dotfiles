f39c7547758f91c5723f4030fe407bb43289479e
bajatin <43754300+bajatin@users.noreply.github.com>
2020-09-17 11:53:57 +0530

    Delete cf.fish

--- fish/functions/cf.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function cf --wraps='~/go/src/github.com/xalanq/cf-tool/cf' --description 'alias cf ~/go/src/github.com/xalanq/cf-tool/cf'
-  ~/go/src/github.com/xalanq/cf-tool/cf  $argv;
-end
